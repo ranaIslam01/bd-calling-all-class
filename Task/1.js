8ec3cff9637f5b76527d5383f2b642541f31f768
Rana Islam <dev.rana.cse@gmail.com>
2025-10-18 18:23:12 +0600

    Task

--- Task/1.js
+++ Task/1.js
@@ -1,43 +1,43 @@
-// // filter odd even
-// const array = [1, 2, 3, 4, 5, 6];
-// const even = array.filter((n) => n % 2 === 0);
-// const odd = array.filter((n) => n % 2 === 1);
-// const evenSum = even.reduce((a, b) => a + b, 0);
-// const oddSum = odd.reduce((a, b) => a + b, 0);
-// console.log("even number: ", even);
-// console.log("odd number: ", odd);
-// console.log("even sum: ", evenSum);
-// console.log("odd sum: ", oddSum);
+// filter odd even
+const array = [1, 2, 3, 4, 5, 6];
+const even = array.filter((n) => n % 2 === 0);
+const odd = array.filter((n) => n % 2 === 1);
+const evenSum = even.reduce((a, b) => a + b, 0);
+const oddSum = odd.reduce((a, b) => a + b, 0);
+console.log("even number: ", even);
+console.log("odd number: ", odd);
+console.log("even sum: ", evenSum);
+console.log("odd sum: ", oddSum);
 
-// //Combine tow array
-// const a = "Rana ";
-// const b = "Rakib";
-// const c = a.concat(b);
-// console.log("Concat",c);
+//Combine tow array
+const a = "Rana ";
+const b = "Rakib";
+const c = a.concat(b);
+console.log("Concat",c);
 
-// // Flaten nested array
+// Flaten nested array
 
-// const arr = [1, 2, [3, 4, [5, 6]]];
-// const flatarray = arr.flat(Infinity);
-// console.log("Flaten: ",flatarray);
+const arr = [1, 2, [3, 4, [5, 6]]];
+const flatarray = arr.flat(Infinity);
+console.log("Flaten: ",flatarray);
 
-// // splice
+// splice
 
-// const splice = [1, 2, 3, 4, 5];
-// const spliceArray = splice.splice(1, 3);
-// console.log("Splice : ",spliceArray);
+const splice = [1, 2, 3, 4, 5];
+const spliceArray = splice.splice(1, 3);
+console.log("Splice : ",spliceArray);
 
-// // Map
-// const objArray = [1, 2, 3, 4];
-// const objMap = objArray.map((n) => n * 2);
-// console.log("Map: ",objMap);
+// Map
+const objArray = [1, 2, 3, 4];
+const objMap = objArray.map((n) => n * 2);
+console.log("Map: ",objMap);
 
-// // Filter
-// const objFilter = objArray.filter((a) => a > 2);
-// console.log("Filter",objFilter);
+// Filter
+const objFilter = objArray.filter((a) => a > 2);
+console.log("Filter",objFilter);
 
-// // Reduce
-// const objReduce = objArray.reduce((a, b) => a + b, 0);
-// console.log("Reduce: ",objReduce);
+// Reduce
+const objReduce = objArray.reduce((a, b) => a + b, 0);
+console.log("Reduce: ",objReduce);
 
 
